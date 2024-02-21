8761fa2ee0aff1fa52c873c7bc878e1ad62b6814
Kshitij Kumar <kumarkshitij171@gmail.com>
2024-02-22 01:59:39 +0530

    Add react-responsive-modal and dependencies

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,7 +1,8 @@
 import { useContext, useEffect, useState } from "react"
-import { useParams,useNavigate } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { UserContext } from "../context/UserContext"
-
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 const Post = () => {
 
   const [blogInfo, setBlogInfo] = useState(null)
@@ -16,16 +17,29 @@ const Post = () => {
       .catch(err => console.log(err))
   }, [])
 
-  const editPost = async()=>{
+  const editPost = async () => {
     navigate(`/edit-post/${id}`)
   }
 
-  const deletePost = async()=>{
+  const [open, setOpen] = useState(false);
+  const [modalVal, setModalVal] = useState('');
+  const [PostDel, setPostDel] = useState('delete');
+
+  const onOpenModal = () => setOpen(true);
+  const onCloseModal = () => setOpen(false);
+
+  const modalOpen = () => {
+    onOpenModal()
+
+  }
+
+  const deletePost = async () => {
+    setPostDel('Deleting...')
     const response = await fetch(`http://localhost:8080/delete-post/${id}`, {
       method: 'DELETE',
       credentials: 'include',
     })
-    if(response.status === 200){
+    if (response.status === 200) {
       navigate('/')
     }
   }
@@ -45,9 +59,18 @@ const Post = () => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
               </svg>
 
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-8 cursor-pointer inline-block bg-white rounded-md" onClick={deletePost}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-12 h-8 cursor-pointer inline-block bg-white rounded-md" onClick={modalOpen}>
                 <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
               </svg>
+
+              <Modal open={open} onClose={onCloseModal} center>
+                <div className="m-3 py-1">
+                  <p className="">Are you want to <span className="font-bold text-red-600">'delete'</span> the post.</p>
+                  <p className="my-1">Write delete in input field</p>
+                  <input className="bg-gray-200 p-1 rounded-md" type="text" value={modalVal} onChange={(e)=>setModalVal(e.target.value)} />
+                  <button className="bg-red-600 block p-2 my-2 rounded-lg font-bold text-white" disabled={modalVal !== 'delete'}  onClick={deletePost}>{PostDel}</button>
+                </div>
+              </Modal>
 
             </div>
           }
